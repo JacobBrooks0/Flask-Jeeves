b04d93a8ce01ee86b986423845519bd34c835b9b
ajearle11 <ajearle9@gmail.com>
2023-08-07 17:49:29 +0100

    Added some small things for maps

--- client/src/pages/MapPage/index.jsx
+++ client/src/pages/MapPage/index.jsx
@@ -1,25 +1,25 @@
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./style.module.css";
 import {
   GoogleMap,
   Marker,
   InfoWindow,
   useLoadScript,
+  LoadScript,
+  StandaloneSearchBox,
 } from "@react-google-maps/api";
 
 export default function MapPage() {
+  const libraries = ["drawing", "places"];
   const { isLoaded } = useLoadScript({
-    id: "713a483497249211",
-    googleMapsApiKey: "AIzaSyC12dt0iPt_ASPAXEhjvbdermheQ1_8Gko",
+    googleMapsApiKey: import.meta.env.VITE_MAPS_API_KEY,
+    libraries: libraries,
   });
   const [mapRef, setMapRef] = useState();
+  const [searchValue, setSearchValue] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const [markers, setMarker] = useState([]);
   const [infoWindowData, setInfoWindowData] = useState();
-  const markers = [
-    { address: "Address1", lat: 18.5204, lng: 73.8567 },
-    { address: "Address2", lat: 18.5314, lng: 73.8446 },
-    { address: "Address3", lat: 18.5642, lng: 73.7769 },
-  ];
 
   const onMapLoad = (map) => {
     setMapRef(map);
@@ -34,9 +34,29 @@ export default function MapPage() {
     setIsOpen(true);
   };
 
+  function useLocation() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setMarker([
+        {
+          address: "Your position",
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        },
+      ]);
+    });
+  }
+
+  function hanldePlacesChanged() {}
+
+  // const searchBox = new google.maps.places.SearchBox();
+
+  useEffect(() => {
+    useLocation();
+  }, []);
+
   return (
     <div className={style["header"]}>
-      {!isLoaded ? (
+      {!isLoaded || !markers ? (
         <h1>Loading...</h1>
       ) : (
         <GoogleMap
@@ -44,6 +64,27 @@ export default function MapPage() {
           onLoad={onMapLoad}
           onClick={() => setIsOpen(false)}
         >
+          <StandaloneSearchBox onPlacesChanged={hanldePlacesChanged}>
+            <input
+              type="text"
+              placeholder="Customized your placeholder"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `240px`,
+                height: `32px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+                position: "absolute",
+                left: "50%",
+                marginLeft: "-120px",
+              }}
+            />
+          </StandaloneSearchBox>
           {markers.map(({ address, lat, lng }, ind) => (
             <Marker
               key={ind}
