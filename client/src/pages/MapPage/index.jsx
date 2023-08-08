60a20dfe7dd98d24914a9e7dcedfb8b381c98787
ajearle11 <ajearle9@gmail.com>
2023-08-08 10:30:39 +0100

    Added google map search

--- client/src/pages/MapPage/index.jsx
+++ client/src/pages/MapPage/index.jsx
@@ -7,6 +7,18 @@ import {
   useLoadScript,
   StandaloneSearchBox,
 } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
 export default function MapPage() {
   const [markers, setMarker] = useState([]);
@@ -32,15 +44,30 @@ export default function MapPage() {
 
   useEffect(() => {
     useLocation();
-    setTimeout(() => {
-      setMarker([{ lat: 10, lng: 30 }]);
-      mapRef.current?.panTo(markers[0]);
-    }, 5000);
   }, []);
 
-  const hanldePlacesChanged = () => {
-    null;
+  const hanldePlacesChanged = async (val) => {
+    setValue(val, false);
+    clearSuggestions();
+
+    const results = await getGeocode({ address: val });
+    const { lat, lng } = await getLatLng(results[0]);
+    setMarker([
+      {
+        lat: lat,
+        lng: lng,
+      },
+    ]);
+    mapRef.current?.panTo(markers[0]);
   };
+
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
 
   return (
     <div className={style["header"]}>
@@ -48,8 +75,25 @@ export default function MapPage() {
         <h1>Loading...</h1>
       ) : (
         <div className={style["header"]}>
+          <Combobox onSelect={hanldePlacesChanged}>
+            <ComboboxInput
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              disabled={!ready}
+              className="combobox-input"
+              placeholder="Search office address"
+            />
+            <ComboboxPopover>
+              <ComboboxList>
+                {status === "OK" &&
+                  data.map(({ place_id, description }) => (
+                    <ComboboxOption key={place_id} value={description} />
+                  ))}
+              </ComboboxList>
+            </ComboboxPopover>
+          </Combobox>
           <GoogleMap
-            zoom={10}
+            zoom={13}
             onLoad={onLoad}
             center={markers[0]}
             mapContainerClassName={style["map-container"]}
