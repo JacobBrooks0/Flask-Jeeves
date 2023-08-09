eefff87674298ae111eb51724ead1fd2c7587f3c
Roberta <132603728+Bibi0506@users.noreply.github.com>
2023-08-09 18:55:14 +0100

    db working, added other routes

--- application/user/routes.py
+++ application/user/routes.py
@@ -1,26 +1,45 @@
 from application import db
 from flask import request, jsonify, Blueprint
 from application.models import User
+from flask import render_template
+
 
 user = Blueprint("user", __name__)
+@user.route('/')
+def index():
+    users = User.query.all()
+    return render_template('user/index.html', users=users)
 
-# Route to Create a New User(add data to database w. POST route:)
+# Route to Create a New User (add data to database w. POST route)
 @user.route('/user', methods=['POST'])
 def create_user():
-    #retrieved data from client 
-    data = request.json
-    #created new user using the data
+    # Retrieve data from form
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+    pets = request.form['pets']
+    dob = request.form['dob']
+    appointment_history = request.form['appointment_history']
+
+    # Create a new user using the form data
     new_user = User(
-        first_name=data['first_name'],
-        last_name=data['last_name'],
-        email=data['email'],
-        password=data['password']
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password,
+        pets=pets,
+        dob=dob,
+        appointment_history=appointment_history
     )
-    #send user to DB
+
+    # Send user to DB
     db.session.add(new_user)
     db.session.commit()
-    #return JSON response to the client 
+
+    # Return JSON response to the client
     return jsonify({"message": "User created successfully!"}), 201
+
 
 # Route to Get All Users
 @user.route('/users', methods=['GET'])
@@ -39,7 +58,7 @@ def get_all_users():
 
 # Route to Get a Specific User by ID
 @user.route('/user/<id>', methods=['GET'])
-def get_user_by_id(user_id):
+def get_user_by_id(id):
     user = User.query.filter_by(id=id).first()
     user_data = {
         'id': user.id,
@@ -51,8 +70,8 @@ def get_user_by_id(user_id):
 
 # Route to Update a User by ID
 @user.route('/user/<id>', methods=['PUT'])
-def update_user_by_id(user_id):
-    user = User.query.get_or_404(user_id)
+def update_user_by_id(id):
+    user = User.query.get_or_404(id)
     data = request.get_json()
     user.first_name = data['first_name']
     user.last_name = data['last_name']
@@ -63,8 +82,8 @@ def update_user_by_id(user_id):
 
 # Route to Delete a User by ID
 @user.route('/user/<id>', methods=['DELETE'])
-def delete_user_by_id(user_id):
-    user = User.query.get_or_404(user_id)
+def delete_user_by_id(id):
+    user = User.query.get_or_404(id)
     db.session.delete(user)
     db.session.commit()
     return jsonify({"message": "User deleted successfully!"}), 200
