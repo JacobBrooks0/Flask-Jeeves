770a839551546501a380dbcd45f7125913020128
Jacob <Jacobabrooks04@gmail.com>
2023-08-15 12:26:32 +0100

    Added coverage

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     environment: "jsdom",
     coverage: {
       provider: 'v8' ,
-      reporter: ['default','text', 'json', 'html']
+      reporter: ['text', 'json', 'html']
   },
 
     },
